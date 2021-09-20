0f616fef7bfa3698334584408faa383dd938661b
Milan Stojanovic <milanstoj94@gmail.com>
2021-09-20 17:36:57 -0500

    vector implementation cont

--- lessons/data-structures/arrays/vector-implementation/code/vector.c
+++ lessons/data-structures/arrays/vector-implementation/code/vector.c
@@ -8,12 +8,41 @@
 #include "vector.h"
 
 int main() {
-
-	vector v = {50};
+	vector v;
 	v.init = init;
 	v.init(&v);
-	printf("%d", v.arr[40]);
-	printf("%d", v.capacity);
+
+	printf("The capacity of vector = %d\n", v.capacity);
+	printf("Value at index 10 = %d\n", v.at(&v, 10)); 
+	printf("Vector is empty? (Should be true) %d\n", v.is_empty(&v));
+
+	v.push(&v, 2);
+	v.push(&v, 5);
+	v.push(&v, 12);
+	v.push(&v, 15);
+	v.push(&v, 2);
+	v.push(&v, 8);
+	v.push(&v, 1);
+	v.push(&v, 24);
+
+	v.print(&v);
+	printf("\nVector is empty? (Should be false) %d\n", v.is_empty(&v));
+	printf("Value at index 4 = %d\n", v.at(&v, 4)); 
+	printf("The size of vector: %d\n", v.size);
+
+	v.insert(&v, 4, 69);
+
+	printf("Value at index 4 = %d\n", v.at(&v, 4)); 
+	v.print(&v);
+
+	v.prepend(&v, 69);
+	v.print(&v);
+	int popped = v.pop(&v);
+	printf("%d\n", popped);
+	v.print(&v);
+
+	v.delete(&v, 100);
+	v.print(&v);
 
 	return 0;
 }
--- lessons/data-structures/arrays/vector-implementation/code/vector.h
+++ lessons/data-structures/arrays/vector-implementation/code/vector.h
@@ -1,11 +1,96 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define INIT_CAPACITY 16;
+
 typedef struct vector {
-	int capacity;
+	// struct members
+	int *arr;
 	int size;
-	int* arr;
+	int capacity;
+
+	// struct methods
 	void (*init)(struct vector*);
-	void (*get)(struct vector*);
+	bool (*is_empty)(struct vector*);
+	int (*at)(struct vector*, int index);
+	void (*push)(struct vector*, int item);
+	void (*insert)(struct vector*, int index, int item);
+	void (*prepend)(struct vector*, int item);
+	int (*pop)(struct vector*);
+	void (*delete)(struct vector*, int index);
+	// void (*remove)(struct vector*, int item);
+	// int (*find)(struct vector*, int item);
+	// void (*resize)(struct vector*, int new_capacity);
+	void (*print)(struct vector*);
 } vector ;
 
-void init(vector* v) {
+int at(vector *v, int index) {
+	if (index < 0 || index > v->size) {
+		return 0;
+	} else {
+		return v->arr[index];
+	}
+}
+
+bool is_empty(vector *v) {
+	return (v->size == 0) ? true : false;
+}
+
+void push(vector *v, int item) {
+	v->arr[v->size] = item;
+	v->size++;
+	return;
+}
+
+void insert(vector *v, int index, int item) {
+	for (int i = v->size + 1; i > index; i--) {
+		v->arr[i] = v->arr[i - 1];
+	}
+	v->arr[index] = item;
+	v->size++;
+	return;
+}
+
+void prepend(vector *v, int item) {
+	for (int i = v->size + 1; i > 0; i--) {
+		v->arr[i] = v->arr[i - 1];
+	}
+	v->arr[0] = item;
+	v->size++;
+} 
+
+void print(vector *v) {
+	for (int i = 0; i < v->size; i++) {
+		printf("%d ", v->arr[i]);
+	}
+	printf("\n");
+}
+
+int pop(vector *v) {
+	int pop = v->arr[v->size - 1];
+	v->arr[v->size - 1] = NULL;
+	v->size--;
+	return pop;
+}
+
+void delete(vector *v, int index) {
+	for(int i = index; i < v->size; i++) {
+		v->arr[i] = v->arr[i+1];
+	}
+	v->size--;
+}
+
+void init(vector *v) {
+	v->capacity = INIT_CAPACITY;
+	v->size = 0;
 	v->arr = (int*)malloc(sizeof(int) * v->capacity);
+	v->at = at;
+	v->is_empty = is_empty;
+	v->push = push;
+	v->insert = insert;
+	v->print = print;
+	v->prepend = prepend;
+	v->pop = pop;
+	v->delete = delete;
+	return;
 }
