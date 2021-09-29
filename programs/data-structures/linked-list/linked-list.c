3c24d5e11d2e2a29b19ab91c16d6c35c84c0ddb1
Milan Stojanovic <milanstoj94@gmail.com>
2021-09-29 15:41:08 -0500

    format linked-list.c

--- programs/data-structures/linked-list/linked-list.c
+++ programs/data-structures/linked-list/linked-list.c
@@ -1,26 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
 
-struct node {
+struct node
+{
     int data;
     struct node *next;
 };
 
-struct node *head;  // head pointer holds the address of starting node
+struct node *head; // head pointer holds the address of starting node
 
-int createLinkedList() {
+int createLinkedList()
+{
     struct node *newnode, *temp;
-    newnode = (struct node*)malloc(sizeof(struct node));    // create new node
-    printf("\n\n\t Enter the data for the linkedlist: ");   // enter data
+    newnode = (struct node *)malloc(sizeof(struct node)); // create new node
+    printf("\n\n\t Enter the data for the linkedlist: "); // enter data
     scanf("%d", &newnode->data);
     newnode->next = NULL;
-    if(head == NULL) {
+    if (head == NULL)
+    {
         head = newnode;
         return 1;
-    } else {
+    }
+    else
+    {
         temp = head;
-        while(temp->next != NULL) {
+        while (temp->next != NULL)
+        {
             temp = temp->next;
         }
         temp->next = newnode;
@@ -28,39 +34,47 @@ int createLinkedList() {
     }
 }
 
-void display() {
+void display()
+{
     struct node *temp;
-    if(head != NULL) {
-        for (temp = head; temp != NULL; temp = temp->next) {
+    if (head != NULL)
+    {
+        for (temp = head; temp != NULL; temp = temp->next)
+        {
             printf("%d\t", temp->data);
         }
-    } else {
+    }
+    else
+    {
         printf("\n\n\t The list is empty...");
     }
 }
 
-int main() {
+int main()
+{
     int ch;
-    head = NULL;    // list is empty.
-    do {
+    head = NULL; // list is empty.
+    do
+    {
         printf("\n\n\t 1. Create linked list");
         printf("\n\n\t 2. Display linked list");
         printf("\n\n\t 3. Exit");
         printf("\n\n\t Enter your choice:- ");
         scanf("%d", &ch);
-        switch(ch) {
-            case 1:
-                createLinkedList();
-                break;
-            case 2:
-                display();
-                break;
-            case 3:
-                exit(1);
-                break;
-            default:
-                printf("\n\n\t Wrong entry try again....,");
-                break;
+        switch (ch)
+        {
+        case 1:
+            createLinkedList();
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            exit(1);
+            break;
+        default:
+            printf("\n\n\t Wrong entry try again....,");
+            break;
         }
-    } while(ch!=3);
+    } while (ch != 3);
 }
